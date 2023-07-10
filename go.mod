eded0e5cbbb80b5dd914815c993605c765e84405
michaels1337 <skigin98@gmail.com>
2023-07-10 21:53:34 +0300

    update1

--- go.mod
+++ go.mod
@@ -1,3 +1,7 @@
 module github.com/michaels1337/Myproject
 
 go 1.20
+
+require github.com/michaels1337/puppy v0.0.0-20230710180442-bd10eb636b2e
+
+require github.com/michaels1337/dog v0.0.0-20230710180059-785671462307 // indirect
--- main.go
+++ main.go
@@ -1,8 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/michaels1337/puppy"
+	"github.com/michaels1337/puppy/kitty"
+)
 
 func main() {
-	fmt.Println("New Project")
+	s := puppy.Bark()
+	s1 := puppy.Barks()
+	fmt.Println(s)
+	fmt.Println(s1)
+
+	// also
+	fmt.Println(puppy.Bark())
+	fmt.Println(puppy.Barks())
+
+	//
+	fmt.Println(kitty.Meow())
+	fmt.Println(puppy.BigBark())
+	fmt.Println(puppy.BigBarks())
+
+	// Version 1.0.0
 
 }
